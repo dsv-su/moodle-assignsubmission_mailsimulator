fe3b30c59da694bacfdbc2f4c2d89ec3d0454cde
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2013-12-12 19:28:18 +0400

    Add swedish lang file

---
+++ lang/sv/assignsubmission_mailsimulator.php
@@ -0,0 +1,119 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignsubmission_mailsimulator', language 'sv'
+ *
+ * @package assignsubmission_mailsimulator
+ * @copyright 2013 Department of Computer and System Sciences,
+ *					Stockholm University  {@link http://dsv.su.se}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addalternativemail'] = 'Lägg till ett alternativt mail';
+$string['addalternativemail_help'] = '<p>Varje gång en ny student besöker uppgiften för första gången tilldelas studenten en slumpmässig uppsättning mail. Ju fler alternativa mail som finns, desto mindre troligt är det att två studenter tilldelas samma mail.</p><p> Detta gör det svårare för studenter att fuska.</p>';
+$string['addcontacts'] = 'Lägg till kontakt';
+$string['addmail'] = 'Lägg till mail';
+$string['addnewcontact'] = 'Lägg till ny kontakt';
+$string['addonecontact'] = 'Du behöver lägga till minst en kontakt';
+$string['backtostart'] = 'Tillbaka till uppgiftens startsida';
+$string['contactinuse'] = 'Denna kontakt används och kan därför inte raderas.';
+$string['contact'] = 'Kontakt';
+$string['correctiontemplate'] = 'Rättningsmall';
+$string['correctiontemplateadded'] = 'Rättningsmall {$a} har lagts till';
+$string['correctiontemplateupdated'] = 'Rättningsmall {$a} har uppdaterats';
+$string['newcorrectiontemplate'] = 'Ny rättningsmall';
+$string['updatecorrectiontemplate'] = 'Uppdatera rättningsmall / vikt';
+$string['updatecorrectiontemplate_help'] = 'Ändrar rättningsmallen och uppdateras dess vikt.';
+$string['default'] = 'Aktiverad som standard';
+$string['default_help'] = 'Om aktiverad kommer denna inlämningsmetod användas som standard för alla nya uppgifter.';
+$string['defaultnumbermails'] = 'Antal mail';
+$string['defaultnumbermails_help'] = 'Inställning för standardvärdet för  \'Antal mail\' på uppgiftens konfigurationssida.';
+$string['delete'] = 'Radera ett mail';
+$string['delete_help'] = '<p>Raderar helt ett mail och dess bilagor och svar. Om radera-knappen är inaktiverad betyder det att mailet används och därför inte kan raderas. För att förhindra att nya studenter tilldelas mailet, använd papperskorgen istället.</p>';
+$string['deletecontact'] = 'För att radera en kontakt, rensa de tre fälten.';
+$string['editcontacts'] = 'Redigera kontakt';
+$string['editmail'] = 'Redigera mail';
+$string['edittemplate'] = 'Redigera mall';
+$string['enabled'] = 'Mailsimulator';
+$string['enabled_help'] = 'Om aktiverad kan studenterna arbeta med mailsimulatorn.';
+$string['error_mid'] = 'Felaktigt mail id';
+$string['error_edit'] = 'Du kan inte redigera det här mailet.';
+$string['error_setup'] = 'Uppgiften behöver läggas upp korrekt, kontakta din lärare.';
+$string['err_date'] = 'Senare än dagens datum.';
+$string['err_emptymessage'] = 'Meddelande kan inte vara tomt.';
+$string['err_emptysubject'] = 'Ämne kan inte vara tomt.';
+$string['err_reciever'] = 'Välj minst en mottagare.';
+$string['err_template'] = 'Rättningsmallen kan inte vara tom.';
+$string['fail'] = 'Fel';
+$string['filesubmissions'] = 'Mailsimulatorn tillåter bifogade filer';
+$string['filesubmissions_help'] = 'Om aktiverat har studenter möjlighet att bifoga dokument.';
+$string['forward'] = 'Vidarebefordra';
+$string['fwd'] = 'Fwd: ';
+$string['good'] = 'Bra';
+$string['high'] = 'hög';
+$string['inbox'] = 'Inbox';
+$string['low'] = 'låg';
+$string['mail'] = 'Mail';
+$string['mailssent'] = 'Skickade mail:';
+$string['mailadmin'] = 'Mailsimulator Lärarvy';
+$string['mailbox'] = 'Mailbox';
+$string['mailboxes'] = 'Mailboxar';
+$string['mailsimulator'] = 'Mailsimulator-inlämning';
+$string['mailsimulatorfilename'] = 'mailsubmission.html';
+$string['mailtostudent'] = 'STUDENTENS E-POST-ADDRESS';
+$string['maxattachments'] = 'Storleksbegränsning på bilaga';
+$string['maxattachments_help'] = 'Sätter maximal storlek för bifogade filer. Som standard används storleksbegränsningen för kursen.';
+$string['maxweight'] = 'Maxvikt per mail';
+$string['maxweight_help'] = 'Sätter maximal vikt för ett i en uppgift. Vikterna används som assistans när en uppgift betygssätts.';
+$string['medium'] = 'medium';
+$string['message'] = 'Meddelande';
+$string['newmail'] = 'Nytt mail';
+$string['noanswer'] = 'Inget svar';
+$string['noreplies'] = 'Inga mail har skickats';
+$string['pluginname'] = 'Mailsimulator';
+$string['printed'] = 'Utskriven';
+$string['priority'] = 'Prioritet';
+$string['re'] = 'Re: ';
+$string['recieved'] = 'Mottaget';
+$string['reply'] = 'Svara';
+$string['reply_help'] = '<p>Genom att svara på ett mail simuleras en pågående mailkonversation mellan två eller flera personer.</p>';
+$string['replyall'] = 'Svara alla';
+$string['replyall_help'] = 'Genom att klicka på den här knappen kommer ett svar att skickas till alla kontater inkluderade i konversationen.';
+$string['rating'] = 'Rating';
+$string['returnmailbox'] = 'Återgå till mailboxvy';
+$string['send'] = 'Skicka';
+$string['sent'] = 'Skickad';
+$string['studentreplies'] = 'Svar:';
+$string['studentnewmails'] = 'Nya mail:';
+$string['subject'] = 'Ämne';
+$string['teacherid'] = 'Lärarmail';
+$string['teacherid_help'] = 'Denna lärare kommer läggas till i listan över sändare/mottagare.';
+$string['timesent'] = 'Tidpunkt skickad';
+$string['trash'] = 'Papperskorg';
+$string['trashrestore'] = 'Papperskorg/återställ';
+$string['trashrestore_help'] = '<p>Ett mail kan deaktiveras genom att lägga det i papperskorgen. Detta innebär att nya studenter inte kommer tilldelas mailet när du öppnar uppgiften första gången.</p><p> För att aktivera mailet igen, gå till papperskorgen och klicka på återställningsknappen för mailet.</p>';
+$string['weight'] = 'Vikt';
+$string['weightgiven'] = 'Given vikt:';
+$string['weight_help'] = "<p>Vikterna är till för att bistå vid betygssättning, vikten avgör hur mycket ett mail är värt.</p>
+<p>Om en student ger ett bra svar multipliceras vikten med 2, om svaret är OK multipliceras det 1 och om svaret
+är fel multipliceras det med 9.</p><p> Exempel på en uppgift med tre mail:<br />
+    5 (vikt) x 2 (Bra) = 10<br />
+    3 (vikt) x 1 (OK) = 3<br />
+    4 (vikt) x 0 (Fel) = 0<br /><br />
+    Total vikt är 13 av 24 möjliga.</p>";
+$string['weight_maxweight'] = 'Vikt/Max';
+$string['wrote'] = '{$a} skrev';
